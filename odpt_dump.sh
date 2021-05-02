7f58a1cc7f7dc803e9c10ca5f5e9ef0d68d2184a
uemuraj <uemura.junya@gmail.com>
2021-05-02 15:53:27 +0900

    基本になるビューを作成。

--- odpt_dump.sh
+++ odpt_dump.sh
@@ -6,8 +6,8 @@ RDF_TYPE=$1
 table_dump=$(curl -sS -L https://api-tokyochallenge.odpt.org/api/v4/odpt:$RDF_TYPE.json?acl:consumerKey=${ACL_CONSUMERKEY:?} | jq -c ".[]")
 table_name=${RDF_TYPE,,}
 
-echo "\\timing on"
 echo "CREATE TABLE IF NOT EXISTS ${table_name} (data jsonb NOT NULL);"
+echo "CREATE UNIQUE INDEX ON ${table_name} ((data->>'owl:sameAs'));"
 echo "TRUNCATE TABLE ${table_name};"
 
 IFS=$'\n'
